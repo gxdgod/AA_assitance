c6dc0fda8e9731a76a8e98881f5a45dd0f30c0fa
gxd_god3 <gxd_god@foxmail.com>
2015-11-03 21:36:23 +0800

    Slider

---
+++ app/src/main/java/mobidever/godutch/controls/SliderMenuItem.java
@@ -0,0 +1,30 @@
+package mobidever.godutch.controls;
+
+/**
+ * Created by leon on 15-11-3.
+ */
+public class SliderMenuItem   {
+
+    private int mItemID;
+    private String mTitle;
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public void setItemID(int mItemID) {
+        this.mItemID = mItemID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public int getItemID() {
+        return mItemID;
+    }
+
+
+
+
+}
